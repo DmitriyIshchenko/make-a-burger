7ff5d13fa0e718a70af950faecfefcb69c3b4847
DmitriyIshchenko <to.dmitry.ishchenko@gmail.com>
2022-12-20 22:56:36 +0300

    feat: create data model

---
+++ src/js/model.js
@@ -0,0 +1,50 @@
+export const state = {
+  recipe: {
+    ingredients: {
+      "bun-top": {
+        name: "bun-top",
+        quantity: 0,
+        calories: 0,
+        mass: 0,
+        time: 0,
+      },
+      bun: {
+        name: "bun",
+        quantity: 0,
+        calories: 0,
+        mass: 0,
+        time: 0,
+      },
+      "bun-bottom": {
+        name: "bun-bottom",
+        quantity: 0,
+        calories: 0,
+        mass: 0,
+        time: 0,
+      },
+      beef: { name: "beef", quantity: 0, calories: 0, mass: 0, time: 0 },
+      mayo: { name: "mayo", quantity: 0, calories: 0, mass: 0, time: 0 },
+      cheese: { name: "cheese", quantity: 0, calories: 0, mass: 0, time: 0 },
+      tomato: { name: "tomato", quantity: 0, calories: 0, mass: 0, time: 0 },
+      onion: { name: "onion", quantity: 0, calories: 0, mass: 0, time: 0 },
+      pickle: { name: "pickle", quantity: 0, calories: 0, mass: 0, time: 0 },
+      salad: { name: "salad", quantity: 0, calories: 0, mass: 0, time: 0 },
+    },
+    order: ["bun-bottom"],
+  },
+  favorites: [],
+};
+
+export const addIngredient = function (name) {
+  state.recipe.ingredients[name].quantity += 1;
+  state.recipe.order.unshift(name);
+};
+
+export const deleteIngredient = function (name) {
+  state.recipe.ingredients[name].quantity -= 1;
+
+  const index = state.recipe.order.findIndex((item) => item === name);
+  state.recipe.order.splice(index, 1);
+
+  return index;
+};
