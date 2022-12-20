f409786adc763cff09ff8e666881ba9660765d3d
DmitriyIshchenko <to.dmitry.ishchenko@gmail.com>
2022-12-20 22:55:55 +0300

    feat: create ingredients view

---
+++ src/js/views/ingredientsView.js
@@ -0,0 +1,60 @@
+import View from "./View";
+import images from "url:../../img/*.png";
+
+class ingredientsView extends View {
+  _parentElement = document.querySelector(".ingredients");
+
+  addHandlerUpdateQuantity(handler) {
+    this._parentElement.addEventListener("click", function (e) {
+      const btn = e.target.closest(".btn-update-quantity");
+
+      if (!btn) return;
+
+      const { name, updateTo } = btn.dataset;
+
+      if (updateTo < 0) return;
+      handler(name, +updateTo);
+    });
+  }
+
+  _generateMarkup() {
+    return Object.values(this._data)
+      .filter((item) => !item.name.includes("-")) // exclude top and bottom buns
+      .map(this._generateCardMarkup)
+      .join("");
+  }
+
+  _generateCardMarkup(ingredient) {
+    const { name, quantity } = ingredient;
+    return `
+    <div class="card">
+      <div class="card__img-box">
+        <img class="card__img" src="${
+          images[`${name}-single`] || images[name]
+        }" alt="${name}">
+      </div>
+
+      <p class="card__title">${name}</p>
+
+      <div class="card__controls">
+        <button 
+          class="card__btn btn-update-quantity" 
+          data-update-to=${quantity - 1} 
+          data-name=${name}
+          >-
+          </button>
+
+        <span>${quantity}</span>
+
+        <button 
+        class="card__btn btn-update-quantity" 
+        data-update-to=${quantity + 1} 
+        data-name=${name}>+
+        </button>
+      </div>
+    </div>
+    `;
+  }
+}
+
+export default new ingredientsView();
