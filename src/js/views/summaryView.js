90da24697dd358ff4fc86ecd777a8eb1a1af24cd
DmitriyIshchenko <to.dmitry.ishchenko@gmail.com>
2022-12-26 19:36:34 +0300

    feat: create burger page layout

--- src/js/views/summaryView.js
+++ src/js/views/summaryView.js
@@ -5,26 +5,26 @@ class SummaryView extends View {
   _parentElement = document.querySelector(".summary");
   _generateMarkup() {
     return ` 
-    <h2 class="summary__heading">Summary</h2>
-    <span class="summary__price">$${this._data.price.toFixed(2)}</span>
+    <h2 class="summary__heading heading-secondary">Summary</h2>
+    <span class="summary__price">&dollar;${this._data.price.toFixed(2)}</span>
     <button class="summary__btn">Checkout</button>
 
-    <p class="summary__message">Build a 
-      <span class="summary__message--accent">$10</span> 
+    <p class="summary__message paragraph">Build a 
+      <span class="paragraph--accent">&dollar;10</span> 
     burger and get a gift!</p>
 
     <div class="summary__details">
-      <div class="summary__time">
+      <div>
         <svg class="summary__icon summary__icon--time">
           <use href="${icons}#time"></use>
         </svg>
         <span>${this._data.time} min</span></div>
-      <div class="summary__mass">
+      <div>
         <svg class="summary__icon summary__icon--mass">
           <use href="${icons}#scales"></use>
         </svg>
         <span>${this._data.mass} g</span></div>
-      <div class="summary__calories">
+      <div>
         <svg class="summary__icon summary__icon--fire">
           <use href="${icons}#fire"></use>
         </svg>
