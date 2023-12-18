dac0cb670ba5280627f424bcbf9717963e110054
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2023-12-18 22:11:39 +0300

    feat: generate navigation

--- src/js/views/UI/navigationView.js
+++ src/js/views/UI/navigationView.js
@@ -1,27 +1,73 @@
 import View from '../View';
 
-class TabView extends View {
-  _navParent = document.querySelector('.nav');
+class NavigationView extends View {
+  _parentSelector = '.header';
+  _className = 'nav';
+  _tag = 'nav';
 
-  _burgerParent = document.querySelector('.burger-demo');
+  _parent;
+  _element;
 
-  _mobileNavBtn = document.querySelector('.mobile-nav-btn');
+  constructor() {
+    super();
 
+    this._parent = document.querySelector('.header');
+    this._element = this._mount();
+
+    this._handleToggleMobileNav();
+  }
+
+  _generateMarkup() {
+    return `
+      <ul class="nav__list">
+        <li class="nav__item"><a href="#discover" class="nav__link nav__link--active">Discover</a></li>
+        <li class="nav__item">
+          <a href="#create-burger" class="nav__link">Make Your Burger</a>
+        </li>
+        <li class="nav__item nav__phone-number-box">
+          <a href="tel:+88004378722" class="nav__phone-number">8 800
+            437-87-22</a>
+        </li>
+        <li class="nav__item nav__user">
+          <picture>
+            <source srcset="src/img/emojis/user.webp" type="image/webp">
+            <source srcset="src/img/emojis/user.png" type="image/png">
+            <img src="src/img/emojis/user.png" alt="user image" class="nav__user-img">
+          </picture>
+          <p class="nav__user-name">John Doe</p>
+        </li>
+      </ul>
+    `;
+  }
+
+  _handleToggleMobileNav() {
+    const mobileNav = document.querySelector('.mobile-nav-btn');
+    mobileNav.addEventListener('click', () => this._toggleMobileNav());
+    this._element.addEventListener('click', (e) => {
+      const link = e.target.closest('.nav__link');
+      if (!link) return;
+
+      this._toggleMobileNav();
+      this._updateActiveLink(link);
+    });
+  }
+
+  _updateActiveLink(activeLink) {
+    const links = document.querySelectorAll('.nav__link');
+    links.forEach((link) => link.classList.remove('nav__link--active'));
+
+    activeLink.classList.add('nav__link--active');
+  }
+
+  _toggleMobileNav() {
+    this._element.classList.toggle('nav-open');
+  }
+
+  /*
   addHandlerToggleTabs() {
     [this._burgerParent, this._navParent].forEach((elem) =>
       elem.addEventListener('click', this._toggleTabs.bind(this))
     );
-  }
-
-  addHandlerToggleMobileNav() {
-    this._mobileNavBtn.addEventListener(
-      'click',
-      this._toggleMobileNav.bind(this)
-    );
-  }
-
-  _toggleMobileNav() {
-    this._navParent.classList.toggle('nav-open');
   }
 
   _toggleTabs(e) {
@@ -47,6 +93,7 @@ class TabView extends View {
     // Close mobile nav
     this._navParent.classList.remove('nav-open');
   }
+  */
 }
 
-export default new TabView();
+export default NavigationView;
